6a9f0bd60d68a0b31530f796bef5581afa8afa08
Graeme64 <ashtwarf16996@ccps.org>
2026-01-28 11:25:55 -0500

    Create acronyms.js

---
+++ acronyms.js
@@ -0,0 +1,24 @@
+// acronyms.js (or inside a <script> tag)
+const acronyms = [
+  {
+    acronym: "AAA",
+    words: ["Authentication", "Authorization", "Accounting"]
+  },
+  {
+    acronym: "ACL",
+    words: ["Access", "Control", "List"]
+  },
+  {
+    acronym: "AES",
+    words: ["Advanced", "Encryption", "Standard"]
+  },
+  {
+    acronym: "ALE",
+    words: ["Annualized", "Loss", "Expectancy"]
+  },
+  {
+    acronym: "AP",
+    words: ["Access", "Point"]
+  }
+  // Add many more from your SY0-701 list
+];
